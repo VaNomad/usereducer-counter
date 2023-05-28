72e2e5c673564372c54d9db96cfaa682ec95eef7
VaNomad <sjur.hassel@gmail.com>
2023-05-28 20:31:24 +0100

    Vite/Tailwind Setup + eslint rule overrides

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen min-w-[320px] m-0 flex justify-center items-center">
+        <h1 className="text-6xl font-bold">Hi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
